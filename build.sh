d40228e9e194227fcd50c851055ef0ddb017b2aa
Dan Burke <dburke@addictmud.org>
2015-02-14 11:33:14 -0500

    make sure base image is up to date on build

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker pull centos:centos6
+
 docker build -t=dwburke/centos-erlang:latest .
 #docker tag `docker images | grep dwburke/centos-rabbitmq-3.3.4 | grep latest | awk '{ print $3 }'` jamasia:5000/rabbitmq-3.3.4 
 
