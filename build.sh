820e8e2b11a3f01d843279c7a4179124c12d1813
Dan Burke <dburke@addictmud.org>
2015-03-21 11:40:06 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 docker pull centos:centos6
 
 docker build -t=dwburke/centos-erlang:latest .
-#docker tag `docker images | grep dwburke/centos-rabbitmq-3.3.4 | grep latest | awk '{ print $3 }'` jamasia:5000/rabbitmq-3.3.4 
 
